3276935b8b0457a0fa744fc5b1fb24dd434c4337
KuKriK <ilapolityko@gmail.com>
2024-11-06 16:02:31 +0300

    Delete LABA1.2PPOIS

--- LABA1.2PPOIS/Knife.cpp
+++
@@ -1,7 +0,0 @@
-#include "Knife.h"
-void Knife::setName(string name) {
-	this->name = name;
-}
-string Knife::getName() {
-	return name;
-}
--- LABA1.2PPOIS/Slice.cpp
+++
@@ -1,4 +0,0 @@
-#include "Slice.h"
-string Slice::getState(){
-	return to_string(ingridient.second) + ' ' + ingridient.first + " г. был(а) нарезана " + name + "ом";
-}
